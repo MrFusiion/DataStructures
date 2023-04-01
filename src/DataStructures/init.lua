4ae036ecbb549ee2dfcde48b960f19cbff819b68
MrFusiion <71613276+MrFusiion@users.noreply.github.com>
2023-04-01 05:59:01 +0200

    Create init.lua

---
+++ src/DataStructures/init.lua
@@ -0,0 +1,11 @@
+return {
+    ArrayList = require(script.ArrayList),
+    BinaryHeap = require(script.BinaryHeap),
+    CircularBuffer = require(script.CircularBuffer),
+    LinkedList = require(script.LinkedList),
+    PriorityQueues = require(script.PriorityQueues),
+    Queue = require(script.Queue),
+    SortedArray = require(script.SortedArray),
+    Stack = require(script.Stack),
+    Types = require(script.Types)
+}
